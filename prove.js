b05d9b5aff61b652a1a1d6eb11661f5a286bef27
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-07-12 19:49:39 +0100

    WIP

--- prove.js
+++ prove.js
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import { execSync } from 'child_process';
 import { loadCompressedPublicKey } from './keygen.js';
-
-const json = JSON.parse(fs.readFileSync('./temp/main.json', 'utf8'));
+import json from './temp/main.json' with { type: 'json' };
 
 // Load pubKey buffer from hex string
 const pubKey = Buffer.from(json.pubKey, 'hex');
@@ -13,6 +12,7 @@ public_key_x = [${pairs.getPublic().getX().toArray().join(', ')}]
 public_key_y = [${pairs.getPublic().getY().toArray().join(', ')}]
 signature = [${Buffer.from(json.signaure, 'hex').join(', ')}]
 root = "${json.root}"
+triple = { terms = [${json.triples[0].map(x => `"${x}"`).join(', ')}], path = [${json.paths[0].map(x => `"${x}"`).join(', ')}], directions = [${json.direction[0].map(x => x.toString()).join(', ')}] }
 `);
 
 console.log(execSync('cd noir_prove && nargo execute', { stdio: 'pipe' }).toString());
